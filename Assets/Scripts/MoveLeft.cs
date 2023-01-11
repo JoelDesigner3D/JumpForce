f2120322ac4368a8e8d233c50d7396af8fa3098c
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-01-11 14:44:22 +0100

    Dont stack obstacles after the game over

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,10 +6,26 @@ public class MoveLeft : MonoBehaviour
 {
 
     public float speed = 30;
+    private float leftBound = -15f;
+    private PlayerController playerControllerScript;
+
+    private void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
